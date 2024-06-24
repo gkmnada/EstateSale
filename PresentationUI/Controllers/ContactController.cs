637ec4de3b1956e6b084641fd8c82a1a68566340
gkmnada <gkmenada@hotmail.com>
2024-06-24 13:58:08 +0300

    ESTATE

---
+++ PresentationUI/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PresentationUI.Dtos.ContactDto;
+using System.Text;
+
+namespace PresentationUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ContactController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _clientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7071/api/Contact", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ PresentationUI/ViewComponents/Contact/Address.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationUI.ViewComponents.Contact
+{
+    public class Address : ViewComponent
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Address(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
